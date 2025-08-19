cb8bfff53757e83546cdba8791669ebf0483c48d
GiorgioLA <Giorgio.Antunes@sptech.school>
2025-08-19 01:32:10 -0300

    1.4.3 tela de caracteristicas evoluindo

--- api-front-end/src/Pages/Caracteristicas/Caracteristicas.jsx
+++ api-front-end/src/Pages/Caracteristicas/Caracteristicas.jsx
@@ -1,12 +1,61 @@
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
 import { Navbar } from "../../components/Navbar/Navbar"
-import { Seletor } from "../../components/SeletorSubpagina/Seletor"
+import { Seletor } from "../../components/SeletorSubpagina/Seletor";
+import { Paper } from '@mui/material';
+import styles from "./caracteristicas.module.css"
+import axios from 'axios';
+import { useEffect } from 'react';
 
 export function Caracteristicas() {
+
+    const resposta = [
+  {
+    "id": 1,
+    "nome": "Tecido"
+  },
+  {
+    "id": 2,
+    "nome": "Roupa"
+  },
+  {
+    "id": 3,
+    "nome": "Nylon"
+  },
+  {
+    "id": 4,
+    "nome": "Poliéster"
+  },
+  {
+    "id": 5,
+    "nome": "Algodão"
+  }]
 
     return (
         <div>
             <Navbar vazio={false} pageNumber={1} />
-            <Seletor esquerda="Categorias" direita="Características" escolhido={2} paginaUm = "/categorias" paginaDois = "/caracteristicas"/>
+            <Seletor esquerda="Categorias" direita="Características" escolhido={2} paginaUm="/categorias" paginaDois="/caracteristicas" />
+            <div className={styles.main}>
+                <div className={styles.listCategorias}>
+                    <Paper >
+                        <List>{resposta.map((categoria)=>
+                            <ListItem>
+                                {categoria.nome}
+                            </ListItem>
+                        )}</List>
+                    </Paper>
+                </div>
+                                <div className={styles.listCategorias}>
+                    <Paper >
+                        <List>{resposta.map((categoria)=>
+                            <ListItem>
+                                {categoria.nome}
+                            </ListItem>
+                        )}</List>
+                    </Paper>
+                </div>
+            </div>
         </div>
     )
 }
