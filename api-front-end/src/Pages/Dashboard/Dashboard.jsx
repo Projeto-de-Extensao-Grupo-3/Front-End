3c023ee4ee9954bf4bc639b12dac652cd69c5555
JoaoRossii <joao.rossi@sptech.school>
2025-08-25 18:33:17 -0300

    1.5.2 - Dashboard: esquema de flexbox da dashboard

--- api-front-end/src/Pages/Dashboard/Dashboard.jsx
+++ api-front-end/src/Pages/Dashboard/Dashboard.jsx
@@ -6,8 +6,21 @@ export function Dashboard() {
     return( 
         <div>
             <Navbar />
-            <div className={styles.containerDash}>
+            <div className={styles.sectionDash}>
                 <Filtro />
+                <div className={styles.containerDash}>
+                    <div className={`${styles.conteudoDash}`}>
+                        <div className={`${styles.conteudoGraficos}`}>
+                            <h1>Gráfico de Barras</h1>
+                        </div>
+                        <div className={`${styles.conteudoGraficos}`}>
+                            <h1>Card de Defeitos</h1>
+                        </div>
+                    </div>
+                    <div className={`${styles.conteudoDash}`}>
+                        <h1>Gráfico de Linhas</h1>
+                    </div>
+                </div>
             </div>
         </div>
     )
